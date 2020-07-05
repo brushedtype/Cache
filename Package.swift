92a705bc1828ef3a18c9c78460ebf2d34f76ebf1
Ed Wellbrook <edwellbrook@gmail.com>
2020-07-05 19:43:21 +0100

    Update

--- Package.swift
+++ Package.swift
@@ -5,25 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "Cache",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Cache",
-            targets: ["Cache"]),
+    platforms: [
+        .macOS(.v10_11), .iOS(.v13), .tvOS(.v9)
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
+    products: [
+        .library(name: "Cache", targets: ["Cache"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Cache",
-            path: "Source/Shared",
-            exclude: ["Library/ImageWrapper.swift"]), // relative to the target path
-        .testTarget(
-            name: "CacheTests",
-            dependencies: ["Cache"],
-            path: "Tests"),
+        .target(name: "Cache", path: "Source/Shared", exclude: ["Library/ImageWrapper.swift"]),
+        .testTarget(name: "CacheTests", dependencies: ["Cache"], path: "Tests"),
     ]
 )
